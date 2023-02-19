67d482acc4d24be69ec942c24c9f7a290c488744
GaynorBlake <124524876+GaynorBlake@users.noreply.github.com>
2023-02-19 06:59:42 -0500

    driving cost lab

---
+++ P2LAB1_GaynorBlake.py
@@ -0,0 +1,8 @@
+mpg= float(input()) 
+dpg= float(input())
+
+your_value1= (dpg / mpg) * 20
+your_value2= (dpg / mpg) * 75
+your_value3= (dpg / mpg) * 500
+
+print(f'{your_value1:.2f} {your_value2:.2f} {your_value3:.2f}')
